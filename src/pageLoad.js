2f7c362cf293212181b4239825c1ea88f1022fa8
James Rozsypal <jimmerxl@gmail.com>
2022-08-11 15:20:29 -0400

    Add header element and create getWeather.js to store api functions

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -2,16 +2,21 @@ import Sun from "./images/sunny.png";
 
 export default function pageLoad() {
   const container = document.createElement("div");
-  const gotWeatherP = document.createElement("p");
+  const header = document.createElement("header");
+  const gotWeatherTitle = document.createElement("h1");
+
+  container.classList.add("container");
 
   const sunIcon = new Image();
+  sunIcon.classList.add("sun-logo");
   sunIcon.src = Sun;
 
-  gotWeatherP.textContent = "Got Weather?";
-  gotWeatherP.classList.add("got-weather");
+  gotWeatherTitle.textContent = "Got Weather?";
+  gotWeatherTitle.classList.add("got-weather");
 
-  container.appendChild(sunIcon);
-  container.appendChild(gotWeatherP);
+  header.appendChild(sunIcon);
+  header.appendChild(gotWeatherTitle);
+  container.appendChild(header);
 
   console.log("I'm the page loader mate!");
 
