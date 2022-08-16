df10774f3e01bdff0a38bd82a5c663b89696ea5f
James Rozsypal <jimmerxl@gmail.com>
2022-08-16 13:15:06 -0400

    Reverse attempted gh-pages changes

--- webpack.common.js
+++ webpack.common.js
@@ -15,7 +15,6 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/weather-app/",
     clean: true,
   },
   module: {
